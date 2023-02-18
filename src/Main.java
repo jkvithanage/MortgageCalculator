2f01f2a9249dc2a4942a078201f2bdf63cc1c89e
jkvitthanage <jkvithana@gmail.com>
2023-02-19 00:35:27 +1100

    Added input validation with flow control

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        double principal = Double.parseDouble(sc.nextLine());
+        System.out.print("Principal ($1K - $1M): ");
+        double principal = sc.nextDouble();
+
+        while (principal < 1000 || principal > 1_000_000) {
+            System.out.println("Enter a number between 1,000 and 1,000,000");
+
+            System.out.print("Principal ($1K - $1M): ");
+            principal = sc.nextDouble();
+        }
 
         System.out.print("Annual Interest Rate: ");
-        double annualRate = Double.parseDouble(sc.nextLine());
+        double annualRate = sc.nextDouble();
+
+        while (annualRate <= 0 || annualRate >= 30) {
+            System.out.println("Enter a value greater than 0 and less than or 30");
+
+            System.out.print("Annual Interest Rate: ");
+            annualRate = sc.nextDouble();
+        }
 
         System.out.print("Period (Years): ");
-        int period = Integer.parseInt(sc.nextLine());
+        int period = sc.nextInt();
+
+        while (period <= 0 || period > 30) {
+            System.out.println("Enter a value between 1 and 30");
+
+            System.out.print("Period (Years): ");
+            period = sc.nextInt();
+        }
 
         double monthlyRate = annualRate / 100 / 12;
         int n = period * 12;
