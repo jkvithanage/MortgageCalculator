7e7291dd1445426461998e85867553cb8b5c56b1
jkvitthanage <jkvithana@gmail.com>
2023-02-19 12:19:23 +1100

    Display payment schedule

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public  static double readNumber(String prompt, double min, double max) {
+        /* This method is used to get user inputs */
         Scanner sc = new Scanner(System.in);
         double value;
 
@@ -19,20 +20,41 @@ public class Main {
         return value;
     }
 
-    public static String calculateMortgage(double principal, double annualRate, int period) {
-        double monthlyRate = annualRate / 100 / 12;
-        int numberOfPayments = period * 12;
+    public static String formatCurrency (double number) {
+        /* Formats a number to a currency */
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        return currency.format(number);
+    }
 
+    public static String calculateMortgage(double principal, double monthlyRate, int numberOfPayments) {
+        /* Calculate mortgage and return as a currency */
         double mortgage = principal * monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments) / (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
 
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        return currency.format(mortgage);
+        return formatCurrency(mortgage);
     }
+
+    public static void paymentSchedule(double principal, int numberOfPayments, double monthlyRate) {
+        /* Display the payment schedule */
+        for (int i = 1; i <= numberOfPayments; i++) {
+            double installment = principal * (Math.pow((1 + monthlyRate), numberOfPayments) - Math.pow((1 + monthlyRate), i)) / (Math.pow((1 + monthlyRate), numberOfPayments) - 1);
+            System.out.println(formatCurrency(installment));
+        }
+    }
+
     public static void main(String[] args) {
         double principal = readNumber("Principal ($1K - $1M): ", 1000, 1_000_000);
         double annualRate = readNumber("Annual Interest Rate: ", 1, 30);
         int period = (int)readNumber("Period (Years): ", 1, 30);
 
-        System.out.println("Mortgage: " + calculateMortgage(principal, annualRate, period));
+        double monthlyRate = annualRate / 100 / 12;
+        int numberOfPayments = period * 12;
+
+        System.out.println("MORTGAGE");
+        System.out.println("--------------");
+        System.out.println("Monthly payments: " + calculateMortgage(principal, monthlyRate, numberOfPayments) + "\n");
+
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("--------------");
+        paymentSchedule(principal, numberOfPayments, monthlyRate);
     }
 }
